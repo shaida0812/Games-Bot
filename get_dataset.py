fdadd871e2e9d6d6d76b8506a3ebdae78c43cb02
whojan <whojan@gmail.com>
2024-09-03 18:47:15 +0300

    Update get_dataset.py
    Bu değişikliklerle, Pillow (PIL) kütüphanesini kullanarak resmi okuyabilir ve yeniden boyutlandırabilirsiniz. imread ve imresize fonksiyonları scipy paketinden kaldırıldığı için PIL kullanımı bu hatayı çözmekte uygun bir alternatif olacaktır.

--- get_dataset.py
+++ get_dataset.py
@@ -1,19 +1,19 @@
-# Arda Mavi
 import os
 import numpy as np
-from tensorflow.keras.utils import to_categorical
-from scipy.ndimage import imread, zoom
+from PIL import Image
+from keras.utils import to_categorical
 from sklearn.model_selection import train_test_split
 
 def get_img(data_path):
     # Getting image array from path:
-    img = imread(data_path)
-    img = zoom(img, (150/img.shape[0], 150/img.shape[1], 1), order=3)  # Resize with interpolation
+    img = Image.open(data_path)
+    img = img.resize((150, 150))  # Resize image
+    img = np.array(img)
     return img
 
 def save_img(img, path):
-    from imageio import imwrite
-    imwrite(path, img)
+    img = Image.fromarray(img)
+    img.save(path)
     return
 
 def get_dataset(dataset_path='Data/Train_Data'):
@@ -21,29 +21,29 @@ def get_dataset(dataset_path='Data/Train_Data'):
     try:
         X = np.load('Data/npy_train_data/X.npy')
         Y = np.load('Data/npy_train_data/Y.npy')
-    except FileNotFoundError:
-        labels = os.listdir(dataset_path)  # Get labels
+    except:
+        labels = os.listdir(dataset_path)  # Geting labels
         X = []
         Y = []
-        count_categori = -1
-        label_dict = {}  # For encoding labels
+        count_categori = [-1, '']  # For encode labels
         for label in labels:
-            datas_path = os.path.join(dataset_path, label)
+            datas_path = dataset_path + '/' + label
             for data in os.listdir(datas_path):
-                img = get_img(os.path.join(datas_path, data))
+                img = get_img(datas_path + '/' + data)
                 X.append(img)
-                # For encoding labels:
-                if label not in label_dict:
-                    count_categori += 1
-                    label_dict[label] = count_categori
-                Y.append(label_dict[label])
+                # For encode labels:
+                if data != count_categori[1]:
+                    count_categori[0] += 1
+                    count_categori[1] = data.split(',')
+                Y.append(count_categori[0])
         # Create dataset:
         X = np.array(X).astype('float32') / 255.
         Y = np.array(Y).astype('float32')
-        Y = to_categorical(Y, count_categori + 1)
+        Y = to_categorical(Y, count_categori[0] + 1)
         if not os.path.exists('Data/npy_train_data/'):
             os.makedirs('Data/npy_train_data/')
         np.save('Data/npy_train_data/X.npy', X)
         np.save('Data/npy_train_data/Y.npy', Y)
     X, X_test, Y, Y_test = train_test_split(X, Y, test_size=0.1, random_state=42)
     return X, X_test, Y, Y_test
+
